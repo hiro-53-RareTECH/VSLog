91629b916bba2120cab2a20945f7471c526d7e51
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-01-07 18:27:25 +0900

    月に応じた学習履歴一覧を取得する処理をusecaseに分離

--- src/analytics/db/read.py
+++ src/analytics/db/read.py
@@ -7,6 +7,15 @@ from ....extensions import db
 from ....models import Field
 from ....models import StudyLog
 
+# 共通のフィルター条件
+def _build_filters(user_id: str, first_day: date | None = None, last_day: date | None = None):
+    filters = [StudyLog.user_id == user_id]
+    if first_day:
+        filters.append(StudyLog.study_date >= first_day)
+    if last_day:
+        filters.append(StudyLog.study_date <= last_day)
+    return filters
+
 # ユーザー毎の全学習履歴取得
 def read_all(user_id: str):
     return StudyLog.query.filter_by(user_id=user_id).all()
@@ -27,14 +36,11 @@ def read_by_date(user_id: str, date: str):
 
 # 月に応じたユーザー毎の学習履歴取得
 def read_by_month(user_id: str, first_day: date, last_day: date):
+    filters = _build_filters(user_id, first_day, last_day)
     logs = (
         db.session.query(StudyLog.study_log_id, StudyLog.study_date, Field.fieldname.label('fieldname'), StudyLog.content, StudyLog.hour)
         .join(Field, StudyLog.field_id == Field.field_id)
-        .filter(
-        StudyLog.user_id == user_id,
-        StudyLog.study_date >= first_day,
-        StudyLog.study_date <= last_day
-        )
+        .filter(*filters)
         .all()
     )
     return logs
