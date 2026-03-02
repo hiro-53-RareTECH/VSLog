1a09211aa893752fb041367e3b419111470f89f1
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-03-02 23:41:47 +0900

    新規登録時のテストケースを追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
+import uuid
 from src import create_app
 from src.config import UnitTestingConfig
+from src.models.users import User
 from src.extensions import db
+from src.usecases.adapters import hash_password
 
 @pytest.fixture()
 def app():
@@ -15,3 +18,17 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+@pytest.fixture()
+def existing_user(app):
+    with app.app_context():
+        user = User(
+            user_id=uuid.uuid4(),
+            username='testuser',
+            email='testemail@gmail.com',
+            password=hash_password('test1234'),
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return user
