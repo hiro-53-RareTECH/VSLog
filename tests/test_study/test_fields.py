f074c23340420dfccc7f92300a52e5e2d9d5f9c2
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-03-14 20:32:35 +0900

    test_fieldsのfixtureを引数に設定してテスト設定

--- tests/test_study/test_fields.py
+++ tests/test_study/test_fields.py
@@ -12,19 +12,20 @@ def test_study_fields_view(app, register_user, auth_client):
     res = auth_client.get(url)
     assert res.status_code == 200
 
-@pytest.mark.parametrize(('fieldname[]', 'color_code[]', 'field_id[]', 'row_action[]', 'message'), (
+@pytest.mark.parametrize(('fieldname', 'color_code', 'field_id', 'row_action', 'message'), (
         # 学習分野の重複エラー
-        ('Docker', '#000000', '', 'new', f'{fieldname}は既に登録されています'),
+        ('Docker', '#000000', '', 'new', 'Dockerは既に登録されています'),
         # 登録（成功）
         ('Python', '#000000', '', 'new', '学習分野の更新に成功しました'),
         # 編集（成功）
-        ('Python', '#000000', '', 'update', '学習分野の更新に成功しました'),
+        ('Docker', '#000000', 1, 'update', '学習分野の更新に成功しました'),
         # 削除（成功）
-        ('Python', '#000000', '', 'delete', '学習分野の更新に成功しました'),
-
+        ('Docker', '#000000', 1, 'delete', '学習分野の更新に成功しました'),
 ))
-def test_study_fields_process(app, register_user, auth_client):
+def test_study_fields_process(app, register_user, auth_client, register_fields, fieldname, color_code, field_id, row_action, message):
     with app.test_request_context():
         url = url_for('study.study_fields_process', user_id=register_user['user_id'])
-    
-    res = auth_client.post(url, )
+    form_data = {'fieldname[]': fieldname, 'color_code[]': color_code, 'field_id[]': field_id, 'row_action[]': row_action}
+    res = auth_client.post(url, data=form_data, follow_redirects=True)
+    assert res.status_code == 200
+    assert message in res.data.decode('utf-8')
