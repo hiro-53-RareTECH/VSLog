e89a0e707d97c86e100a0e1a877935b46660e574
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-03-19 19:28:33 +0900

    スタート画面を追加

--- src/blueprints/auth/routes/__init__.py
+++ src/blueprints/auth/routes/__init__.py
@@ -1,3 +1,4 @@
+from .index import index_view
 from .login import login_view, login_process
 from .signup import signup_view, signup_process
 from .password_reset import password_reset_view, password_reset_process
