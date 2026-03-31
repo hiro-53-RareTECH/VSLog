a5cb31fbb052f2309cc6222e8ebe5658ee67e95d
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-03-31 17:13:40 +0900

    postgresql用のcollation削除変更をmigrationに反映

---
+++ migrations/versions/7679d14bf558_postgresql_drop_collation_from_fieldname.py
@@ -0,0 +1,24 @@
+"""postgresql drop collation from fieldname
+
+Revision ID: 7679d14bf558
+Revises: e41b0cd52439
+Create Date: 2026-03-31 17:10:41.518636
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7679d14bf558'
+down_revision = 'e41b0cd52439'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
